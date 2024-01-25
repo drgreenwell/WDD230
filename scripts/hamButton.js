c491a44b8e1b3e5f8040edf7a9090d5cf71badfa
drgreenwell <dgreenwell.byui.edu>
2024-01-25 17:36:23 -0600

    Added Google Map

--- scripts/hamButton.js
+++ scripts/hamButton.js
@@ -1,9 +1,15 @@
 /* hamButton.js */
 
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('nav');
+const mapFrame = document.querySelector('.map');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    if (navigation.classList.contains('open')) {
+        mapFrame.style.display = 'none';
+    } else {
+        mapFrame.style.display = ''; // Reset to default
+    }
 });
