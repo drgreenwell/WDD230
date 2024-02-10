12970119a2d034e4bfb2f0623e288581de6e9f8c
drgreenwell <dgreenwell.byui.edu>
2024-02-10 16:26:30 -0600

    Added Dark Mode

---
+++ chamber/scripts/darkMode.js
@@ -0,0 +1,9 @@
+// darkMode.js
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    darkModeToggle.classList.toggle('dark-mode');
+});
