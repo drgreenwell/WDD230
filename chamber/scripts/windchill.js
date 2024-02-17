593900b07e5c784937f32399e632c60196c1d955
drgreenwell <dgreenwell.byui.edu>
2024-02-17 12:30:23 -0600

    Added windchill and changed images from jpg to webp. Also changed hero image for this week.

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,15 @@
+// windchill.js
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed > 3.0) {
+        var windChill = Math.round(35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16)));
+        return windChill;
+    } else {
+        return "N/A";
+    }
+}
+
+var temperature = parseFloat(document.getElementById('temperature').innerText);
+var windSpeed = parseFloat(document.getElementById('windSpeed').innerText);
+var windChill = calculateWindChill(temperature, windSpeed);
+document.getElementById('windChill').innerText = windChill;
