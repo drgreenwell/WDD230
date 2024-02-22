09e22b1bf7bafaa3182ed8715e6ddb6c55fd120e
drgreenwell <dgreenwell.byui.edu>
2024-02-22 14:40:20 -0600

    Added counter to main website. Added lazy loading to pictures.

---
+++ scripts/localStorage.js
@@ -0,0 +1,11 @@
+// Get the visit count from localStorage or initialize it to 0 if not exists
+let visitCount = localStorage.getItem('visitCount');
+if (!visitCount) {
+    visitCount = 0;
+}
+// Increment visit count
+visitCount++;
+// Update the visit count display
+document.getElementById('visitCount').textContent = visitCount;
+// Store the updated visit count in localStorage
+localStorage.setItem('visitCount', visitCount);
