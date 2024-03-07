3f914d43c0750f30296cdb8cb138ceb0d1cb0d80
drgreenwell <dgreenwell.byui.edu>
2024-03-07 16:46:50 -0600

    members.js 5

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -25,7 +25,9 @@ document.addEventListener("DOMContentLoaded", function() {
             // Function to generate HTML for a single member in list view
             function generateMemberHTMLList(member) {
                 return `
-                    ${member.name} ${member.address} ${member.phone} Website ${member.membership_level} ${member.additional_info}
+                    <div class="member">
+                        <p>${member.name} ${member.address} ${member.phone} <a href="${member.website}" target="_blank">Website</a> ${member.membership_level} ${member.additional_info}</p>
+                    </div>
                 `;
             }
 
