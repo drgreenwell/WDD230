356d2f44c63bad734c97a0100b50410ef1038e97
drgreenwell <dgreenwell.byui.edu>
2024-03-07 13:04:01 -0600

    Updated prophet's page

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -13,24 +13,28 @@ async function getProphetData() {
 
 function displayProphets(prophets) {
   prophets.forEach((prophet) => {
-    let card = document.createElement('section');
-    card.classList.add('prophet-card'); // Add a class for styling
+    let card = document.createElement('div');
+    card.classList.add('prophet-card');
 
     let fullName = document.createElement('h2');
-    fullName.textContent = `${prophet.name} (${prophet.birth})`; // Include Date of Birth
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
 
-    let placeOfBirth = document.createElement('p');
-    placeOfBirth.textContent = `Place of Birth: ${prophet.placeOfBirth}`; // Include Place of Birth
+    let birthdate = document.createElement('p');
+    birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+
+    let birthplace = document.createElement('p');
+    birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
     let portrait = document.createElement('img');
     portrait.src = prophet.imageurl;
-    portrait.alt = `${prophet.name}'s Portrait`;
+    portrait.alt = `${prophet.name} ${prophet.lastname}'s Portrait`;
     portrait.loading = 'lazy';
     portrait.width = '200'; // Adjust width if necessary
     portrait.height = '250'; // Adjust height if necessary
 
     card.appendChild(fullName);
-    card.appendChild(placeOfBirth);
+    card.appendChild(birthdate);
+    card.appendChild(birthplace);
     card.appendChild(portrait);
 
     cards.appendChild(card);
