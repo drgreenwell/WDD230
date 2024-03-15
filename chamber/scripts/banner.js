33055fac8261a80a926e740d0cc93509c950fb81
drgreenwell <dgreenwell.byui.edu>
2024-03-15 13:17:30 -0500

    Banner edit 2

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,6 +1,6 @@
 // Display banner if it's Monday, Tuesday, or Wednesday
 const today = new Date().getDay();
-if (today >= 1 && today <= 3) {
+if (today >= 1 && today <= 7) {
     const banner = document.getElementById('chamber-banner');
     banner.style.display = 'block';
     const closeButton = document.getElementById('close-banner');
