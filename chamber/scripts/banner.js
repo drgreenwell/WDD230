d704068e1baa3340ccf84d3530cc56d1c9dba745
drgreenwell <dgreenwell.byui.edu>
2024-03-21 14:44:50 -0500

    Fixed banner.js listener error

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -4,7 +4,8 @@ if (today >= 1 && today <= 3) {
     const banner = document.getElementById('chamber-banner');
     banner.style.display = 'block';
     const closeButton = document.getElementById('close-banner');
-    closeButton.addEventListener('click', () => {
+    closeButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default behavior of the button
         banner.style.display = 'none';
     });
 }
